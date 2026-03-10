5ab723168cb770b011a1d35366808607135d87ed
Solovev.Arsenii <pozorn1@yandex.ru>
2026-03-10 09:39:41 +0300

    Set constants

--- source/main.c
+++ source/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+
+#define CHIP8_MEMORY_SIZE	4096
+#define CHIP8_DISPLAY_WIDTH	64
+#define CHIP8_DISPLAY_HEIGHT	32
+#define V_REGISTER_COUNT	16
+#define STACK_DEPTH		16
+#define CHIP8_KEY_COUNT		16
+
 int main() {
 	printf("Hello, Wolrld!\n");
 	return 0;
